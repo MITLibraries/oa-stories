f834a9aabf0cce6cca4ca0201b3e424a01f50fcf
Helen Bailey <hakbailey@gmail.com>
2015-09-10 12:42:52 -0400

    Initial commit

---
+++ parse_data.py
@@ -0,0 +1,95 @@
+import csv
+import json
+import xlrd
+from pprint import pprint
+
+datafile = xlrd.open_workbook('data/original-oacomments_structured_summary.xlsx')
+sheet = datafile.sheet_by_index(0)
+output_file = 'data/comments.txt'
+
+def get_data_array(sheet):
+	RESULTS = []
+	for row in range(1, sheet.nrows):
+		if str(sheet.cell_value(row, 5)) == 'Y':
+			kind = sheet.cell_value(row, 0).encode('utf-8')
+			country = sheet.cell_value(row, 6).encode('utf-8')
+			comment = sheet.cell_value(row, 7).encode('utf-8')
+			article = sheet.cell_value(row, 8).encode('utf-8')
+			RESULTS.append([country, kind, article, comment])
+	return RESULTS
+
+def convert_countries(data, col_num):
+	with open('data/countries.json', 'rb') as data_file:
+		countries = json.load(data_file)
+		for d in data:
+			datum = d[col_num].decode('utf-8')
+			for country in countries:
+				try:
+					if datum in country['altSpellings']:
+						d[col_num] = country['cca3']
+				except:
+					pass
+
+				if datum == country['name']:
+					d[col_num] = country['cca3']
+	return data
+
+def get_data_array_from_csv(csv_file):
+	with open(csv_file, 'rb') as file:
+		reader = csv.reader(file, delimiter=',')
+		data = []
+		numRows = 0
+
+		for row in reader:
+			numRows += 1
+		file.seek(0)
+		
+		for i in range(numRows):
+			data_row = reader.next()
+			data.append(data_row)
+
+	return data
+
+def create_download_json(download_data):
+	"""Takes input OA download data list and creates a JSON object with download data, to be filled in with comment data later.
+	"""
+	RESULTS = {}
+	for d in range(1, len(download_data)):
+		country = download_data[d][0].encode()
+		RESULTS[country] = {'downloads':download_data[d][1], 'num_comments':0}
+	return RESULTS
+
+def add_to_json(json_data, list_data):
+	for row in list_data:
+		country = row[0]
+		if country in json_data:
+			if 'comments' in json_data[country]:
+				json_data[country]['comments'].append(row[3])
+				json_data[country]['num_comments'] += 1
+			else:
+				json_data[country]['kind'] = row[1]
+				json_data[country]['article'] = row[2]
+				json_data[country]['comments'] = [row[3]]
+				json_data[country]['num_comments'] = 1
+	return json_data
+
+def write_to_json(content, file):
+	"""Writes content to a json file.
+
+	Args:
+		content (dict): Content to be written to file.
+		file (string): Path to file to be written, including desired name of file (with extension).
+	"""
+	with open(file, 'wb') as json_file:
+		json.dump(content, json_file, indent=4)
+
+if __name__ == '__main__':
+	comment_data = get_data_array(sheet)
+	comment_data = convert_countries(comment_data, 0)
+
+	download_data = get_data_array_from_csv('data/OA_Stats_Map.csv')
+	download_data = convert_countries(download_data, 0)
+	download_data = create_download_json(download_data)
+
+	json_data = add_to_json(download_data, comment_data)
+	write_to_json(json_data, 'static/comments.json')
---
+++ static/main.js
@@ -0,0 +1,156 @@
+var color_q0 = 'rgb(255,255,255)',
+	color_q1 = 'rgb(184,194,226)',
+	color_q2 = 'rgb(131,145,186)',
+	color_q3 = 'rgb(85,101,152)',
+	color_q4 = 'rgb(47,63,119)',
+	color_q5 = 'rgb(21,30,85)',
+	colorScale = [
+		color_q0,
+		color_q1,
+		color_q2,
+		color_q3,
+		color_q4,
+		color_q5
+	];
+
+var t = d3.scale.threshold()
+	.domain([1, 16, 268, 4393, 71964, 1178682])
+	.range(['q0', 'q1', 'q2', 'q3', 'q4', 'q5']);
+
+var commentData = {}
+
+var map = new Datamap({
+	scope: 'world',
+	element: document.getElementById('map'),
+	// height: null,
+	data: commentData,
+
+	setProjection: function(element) {
+          var projection = d3.geo.equirectangular()
+          	.scale(element.offsetWidth / 7)
+            .translate([ element.offsetWidth / 2, element.offsetHeight / 2 + 50]);
+          var path = d3.geo.path()
+            .projection(projection);
+          
+          return {path: path, projection: projection};
+        },
+	done: function(datamap) {
+		datamap.svg.selectAll('.datamaps-subunit').on('click', function(geo) {
+			d3.selectAll('foreignobject').remove();
+			d3.select("#info")
+				.append("foreignobject")
+				.attr("x", 0)
+				.attr("height", 200)
+				.append("xhtml:body")
+				.attr("class", "infoText")
+				.html(function() {
+					var topLine = '<h2>User comments from ' + geo.properties.name + ':</h2>';
+					var lines = '<ul>';
+					for (item in commentData) {
+						if (geo.id == item) {
+							for (comment in commentData[item].comments) {
+								lines += '<li>' + commentData[item].comments[comment] + '</li>';
+							}
+						}
+					}
+					lines += '</ul>';
+					
+					return topLine + lines;
+				});
+		});
+    },
+	fills: {
+	    defaultFill: 'rgb(255,255,255)',
+	    q0: color_q0,
+	    q1: color_q1,
+	    q2: color_q2,
+	    q3: color_q3,
+	    q4: color_q4,
+	    q5: color_q5
+	 },
+	geographyConfig: {
+		// borderColor: 'rgb(0,0,0)';
+		// },
+		// borderWidth: 2,
+	    popupTemplate: function(geo, data) {
+	      	if (data == null) {
+	        	return '<div class="hoverinfo">No data from ' + geo.properties.name + '</div>';
+	        }
+	      	return '<div class="hoverinfo">' + data.downloads + ' downloads and ' + data.num_comments + ' comments from ' + geo.properties.name + '</div>';
+	    }
+	},
+	responsive: true
+});
+
+window.addEventListener('resize', function() {
+        map.resize();
+    });
+
+var infoBox = d3.select("#container")
+	.append("div")
+	.attr("id", "info");
+
+d3.json('/static/comments.json', function(error, data) {
+
+	function add_fillKey(data) {
+		new_data = data;
+		for (item in new_data) {
+			new_data[item]['fillKey'] = t(data[item]['downloads']);
+		}
+		return new_data;
+	}
+		var commentData = add_fillKey(data);
+	map.updateChoropleth(commentData);
+
+// append the container for the legend
+        d3.select('svg')
+          .append('g')
+          .attr('class', 'legend');
+
+        var
+          legendBarWidth = 30,
+          legendBarHeight = 10,
+          legendOffsetX = 30,
+          legendOffsetY = document.getElementById('map').offsetHeight - 200;
+
+        // draw the legend
+        d3.select('.legend').selectAll('.legend')
+        .data(colorScale)
+        .enter()
+          .append('rect')
+            .attr('x', legendOffsetX)
+            .attr('y', function(d,i) {
+              return legendOffsetY + legendBarHeight*i;
+            })
+            .attr('width', legendBarWidth)
+            .attr('height', legendBarHeight)
+            .attr('fill', function(d){ return d; });
+
+        // draw the legend text labels
+        d3.select('.legend').selectAll('.legend')
+        .data(colorScale)
+        .enter()
+          .append('text')
+            .text(function(d,i) {
+              var quantizedRange = t.invertExtent('q'+i);
+              	if (i == 0) {
+              		return '0 - ' + quantizedRange[1];
+              	} else {
+	              return quantizedRange[0] + ' - ' +
+	              quantizedRange[1];
+          		}
+            })
+            .attr('x', legendOffsetX+(legendBarWidth*1.25))
+            .attr('y', function(d,i){
+              return legendOffsetY + (legendBarHeight*0.9) + legendBarHeight*i;
+            })
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "10px")
+            .attr("fill", "black");
+
+    
+	});
+
+// window.addEventListener('resize', function() {
+//     map.resize();
+// });
