edaa172d4e8ba889f6588f8223521312415ca82a
Helen Bailey <hakbailey@gmail.com>
2015-09-29 12:36:07 -0400

    Add initial comment from random country

--- static/main.js
+++ static/main.js
@@ -33,142 +33,12 @@ var map = new Datamap({
 	data: commentData,
 	done: function(datamap) {
 		datamap.svg.selectAll('.datamaps-subunit')
-		.on('click', function(geo) {
-			d3.selectAll('div.comment').remove();
-			d3.selectAll('svg#country').remove();
-			d3.selectAll('nav').remove();
-
-			var tempData;
-			var tempItem;
-			currIndex = 0;
-
-			for (item in commentData) {
-				if (geo.id == item) {
-					tempData = commentData[item];
-					tempItem = item;
-					if ('comments' in tempData) {
-						maxIndex = tempData.comments.length - 1;
-					} else {
-						maxIndex = 0;
-					}
-				}
-			}
-
-			d3.select('.comment-box')
-				.append('div')
-				.attr('class', 'comment')
-				.append('p')
-				.html(function() {
-					if ('comments' in tempData) {
-						return '<span class="lquote">&ldquo;</span>' + tempData.comments[0] + '<br /><span class="attribute">&mdash; ' + tempData.kind[0] + '</span>';
-					} else {
-						return 'No user comments yet from ' + geo.properties.name;
-					}
-				});
-
-			var gj = {"type": "FeatureCollection","features":[geo]};
-
-			var projection = d3.geo.equirectangular()
-			    .scale(1)
-			    .translate([0, 0]);
-
-			var path = d3.geo.path()
-				.projection(projection);
-
-			var b = path.bounds(gj.features[0]),
-			    s = .95 / Math.max((b[1][0] - b[0][0]) / 200, (b[1][1] - b[0][1]) / 200),
-			    t = [(170 - s * (b[1][0] + b[0][0])) / 2, (200 - s * (b[1][1] + b[0][1])) / 2];
-
-			projection
-				.scale(s)
-				.translate(t);
-
-			d3.select('.country')
-		    	.append('svg')
-		    	.attr('id', 'country')
-		    	.attr('width', 170)
-		    	.attr('height', 200);
-
-			d3.select('#country')
-				.selectAll('path')
-    			.data(gj.features)
-  				.enter()
-  				.append('path')
-    			.attr('d', path)
-    			.attr('fill', 'rgb(131,145,186)');
-
-    		d3.select('#country')
-	    		.append('text')
-	    		.text(function() {
-	    			return geo.properties.name;
-	    		})
-	    		.attr('class', 'wrap')
-	    		.style('text-anchor', 'middle')
-	    		.style('font-size', '1.8em')
-	    		.style('font-weight', 'bold');
-
-	    	d3plus.textwrap()
-    			.container(d3.select(".wrap"))
-    			.width(170)
-    			.x(0)
-    			.y(25)
-    			.draw();
-
-	    	d3.select('#country')
-	    		.append('text')
-	    		.text(function() {
-	    			return tempData.downloads + ' downloads';
-	    		})
-	    		.attr('x', 170/2)
-	    		.attr('y', 168)
-	    		.style('text-anchor', 'middle')
-
-	    	d3.select('#country')
-	    		.append('text')
-	    		.text(function() {
-	    			return tempData.num_comments + ' user comments';
-	    		})
-	    		.attr('x', 170/2)
-	    		.attr('y', 185)
-	    		.style('text-anchor', 'middle');
-
-	    	if (tempData['num_comments'] > 0) {
-		    	d3.select('.dots')
-		    		.append('nav')
-		    		.append('ul')
-		    		.attr('class', 'pagination')
-		    		.append('li')
-		    		.attr('class', 'back-button')
-		    		.classed('disabled', true)
-		    		.html(function() {
-		    			var s = currIndex - 1;
-		    			return '<a onClick="loadPrevious(\'' + tempItem + '\'); return false;" href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a>'
-		    		});
-
-		    	d3.select('.pagination')
-			    	.selectAll('li')
-		    		.data(tempData.comments, function(d, i) {
-		    			return d;
-		    		})
-		    		.enter()
-		    		.append('li')
-		    		.attr('class', function(d, i) {
-		    			return 'num' + i;
-		    		})
-		    		.html(function(d, i) {
-		    			var n = i + 1;
-		    			return '<a onClick="loadComment(\'' + tempItem + '\', + ' + i + '); return false;" href="#">' + n + '</a>';
-		    		});   
-
-		    	d3.select('.pagination')
-		    		.append('li')
-		    		.attr('class', 'next-button')
-		    		.html(function() {
-		    			var s = currIndex + 1;
-		    			return '<a onClick="loadNext(\'' + tempItem + '\'); return false;" href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a>'
-		    		});
-		    };
-		});
+			.on('click', function(geo) {
+				d3.selectAll('div.comment').remove();
+				d3.selectAll('svg#country').remove();
+				d3.selectAll('nav').remove();
+				draw_single_country(geo);
+			});
     },
 	fills: {
 	    defaultFill: 'rgb(255,255,255)',
@@ -199,16 +69,16 @@ window.addEventListener('resize', function() {
 
 d3.json('/static/comments.json', function(error, data) {
 
-	function add_fillKey(data) {
-		new_data = data;
-		for (item in new_data) {
-			new_data[item]['fillKey'] = t(data[item]['downloads']);
-		}
-		return new_data;
-	}
+	commentCountries = [];
 
 	var commentData = add_fillKey(data);
 	map.updateChoropleth(commentData);
+
+	for (item in commentData) {
+		if ('comments' in commentData[item]) {
+			commentCountries.push(item);
+		}
+	}
 
     var
       legendBarWidth = 30,
@@ -259,12 +129,21 @@ d3.json('/static/comments.json', function(error, data) {
         .attr("font-size", "10px")
         .attr("fill", "black");
 
+    // add initial comment from random country
+	var tc = Math.floor((Math.random() * commentCountries.length));
+	var ti = commentCountries[tc];
+
+	d3.select('.datamaps-subunit.' + ti)
+		.attr('class', function(geo) {
+			draw_single_country(geo);
+			return 'hi';
+		});
 	});
 
+// functions
 function loadComment(c, n) {
 	d3.selectAll('div.comment').remove();
 	currIndex = n;
-	console.log(currIndex);
 
 	d3.select('.back-button')
 		.classed('active', false);
@@ -317,6 +196,143 @@ function loadPrevious(c) {
 	loadComment(c, prev);
 }
 
-// window.addEventListener('resize', function() {
-//     map.resize();
-// });
+function add_fillKey(data) {
+	new_data = data;
+	for (item in new_data) {
+		new_data[item]['fillKey'] = t(data[item]['downloads']);
+	}
+	return new_data;
+}
+
+function draw_single_country(geo) {
+	var tempData;
+	var tempItem;
+	currIndex = 0;
+
+	for (item in commentData) {
+		if (geo.id == item) {
+			tempData = commentData[item];
+			tempItem = item;
+			if ('comments' in tempData) {
+				maxIndex = tempData.comments.length - 1;
+			} else {
+				maxIndex = 0;
+			}
+		}
+	}
+
+	d3.select('.comment-box')
+		.append('div')
+		.attr('class', 'comment')
+		.append('p')
+		.html(function() {
+			if ('comments' in tempData) {
+				return '<span class="lquote">&ldquo;</span>' + tempData.comments[0] + '<br /><span class="attribute">&mdash; ' + tempData.kind[0] + '</span>';
+			} else {
+				return 'No user comments yet from ' + geo.properties.name;
+			}
+		});
+
+	var gj = {"type": "FeatureCollection","features":[geo]};
+
+	var projection = d3.geo.equirectangular()
+	    .scale(1)
+	    .translate([0, 0]);
+
+	var path = d3.geo.path()
+		.projection(projection);
+
+	var b = path.bounds(gj.features[0]),
+	    s = .95 / Math.max((b[1][0] - b[0][0]) / 200, (b[1][1] - b[0][1]) / 200),
+	    t = [(170 - s * (b[1][0] + b[0][0])) / 2, (200 - s * (b[1][1] + b[0][1])) / 2];
+
+	projection
+		.scale(s)
+		.translate(t);
+
+	d3.select('.country')
+    	.append('svg')
+    	.attr('id', 'country')
+    	.attr('width', 170)
+    	.attr('height', 200);
+
+	d3.select('#country')
+		.selectAll('path')
+		.data(gj.features)
+			.enter()
+			.append('path')
+		.attr('d', path)
+		.attr('fill', 'rgb(131,145,186)');
+
+	d3.select('#country')
+		.append('text')
+		.text(function() {
+			return geo.properties.name;
+		})
+		.attr('class', 'wrap')
+		.style('text-anchor', 'middle')
+		.style('font-size', '1.8em')
+		.style('font-weight', 'bold');
+
+	d3plus.textwrap()
+		.container(d3.select(".wrap"))
+		.width(170)
+		.x(0)
+		.y(25)
+		.draw();
+
+	d3.select('#country')
+		.append('text')
+		.text(function() {
+			return tempData.downloads + ' downloads';
+		})
+		.attr('x', 170/2)
+		.attr('y', 168)
+		.style('text-anchor', 'middle')
+
+	d3.select('#country')
+		.append('text')
+		.text(function() {
+			return tempData.num_comments + ' user comments';
+		})
+		.attr('x', 170/2)
+		.attr('y', 185)
+		.style('text-anchor', 'middle');
+
+	if (tempData['num_comments'] > 0) {
+    	d3.select('.dots')
+    		.append('nav')
+    		.append('ul')
+    		.attr('class', 'pagination')
+    		.append('li')
+    		.attr('class', 'back-button')
+    		.classed('disabled', true)
+    		.html(function() {
+    			var s = currIndex - 1;
+    			return '<a onClick="loadPrevious(\'' + tempItem + '\'); return false;" href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a>'
+    		});
+
+    	d3.select('.pagination')
+	    	.selectAll('li')
+    		.data(tempData.comments, function(d, i) {
+    			return d;
+    		})
+    		.enter()
+    		.append('li')
+    		.attr('class', function(d, i) {
+    			return 'num' + i;
+    		})
+    		.html(function(d, i) {
+    			var n = i + 1;
+    			return '<a onClick="loadComment(\'' + tempItem + '\', + ' + i + '); return false;" href="#">' + n + '</a>';
+    		});   
+
+    	d3.select('.pagination')
+    		.append('li')
+    		.attr('class', 'next-button')
+    		.html(function() {
+    			var s = currIndex + 1;
+    			return '<a onClick="loadNext(\'' + tempItem + '\'); return false;" href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a>'
+    		});
+    }
+}
