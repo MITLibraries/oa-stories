32fa829bfc2684e72d25c07b221ca7c89da73f7c
Helen Bailey <hakbailey@gmail.com>
2018-07-17 11:03:03 -0400

    Force SSL

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
+from flask_sslify import SSLify
 from raven.contrib.flask import Sentry
 
 app = Flask(__name__)
 sentry = Sentry(app)
+sslify = SSLify(app)
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
