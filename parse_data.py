0083313c6984020975b0ee119afd756887c6fefa
Helen Bailey <hbailey@mit.edu>
2019-08-26 15:14:13 -0400

    Update to Python 3.7, update dependencies, minor bug fix post-updates

--- parse_data.py
+++ parse_data.py
@@ -11,31 +11,31 @@ def get_data_array(sheet):
 	RESULTS = []
 	for row in range(1, sheet.nrows):
 		if str(sheet.cell_value(row, 5)) != 'N':
-			kind = sheet.cell_value(row, 0).encode('utf-8')
-			country = sheet.cell_value(row, 6).encode('utf-8')
-			comment = sheet.cell_value(row, 7).encode('utf-8')
-			article = sheet.cell_value(row, 8).encode('utf-8')
+			kind = sheet.cell_value(row, 0)
+			country = sheet.cell_value(row, 6)
+			comment = sheet.cell_value(row, 7)
+			article = sheet.cell_value(row, 8)
 			RESULTS.append([country, kind, article, comment])
 	return RESULTS
 
 def convert_countries(data, col_num):
-	with open('data/countries.json', 'rb') as data_file:
+	with open('static/countries.json', 'rb') as data_file:
 		countries = json.load(data_file)
 		for d in data:
-			datum = d[col_num].decode('utf-8')
+			datum = d[col_num]
 			for country in countries:
 				try:
 					if datum in country['altSpellings']:
-						d[col_num] = country['cca3']
+						d[col_num] = str(country['cca3'])
 				except:
 					pass
 
 				if datum == country['name']:
-					d[col_num] = country['cca3']
+					d[col_num] = str(country['cca3'])
 	return data
 
 def get_data_array_from_csv(csv_file):
-	with open(csv_file, 'rb') as file:
+	with open(csv_file, 'r') as file:
 		reader = csv.reader(file, delimiter=',')
 		data = []
 		numRows = 0
@@ -45,7 +45,7 @@ def get_data_array_from_csv(csv_file):
 		file.seek(0)
 
 		for i in range(numRows):
-			data_row = reader.next()
+			data_row = next(reader)
 			data.append(data_row)
 
 	return data
@@ -55,7 +55,7 @@ def create_download_json(download_data):
 	"""
 	RESULTS = {}
 	for d in range(1, len(download_data)):
-		country = download_data[d][0].encode()
+		country = download_data[d][0]
 		RESULTS[country] = {'downloads':download_data[d][1], 'num_comments':0}
 	return RESULTS
 
@@ -81,16 +81,16 @@ def write_to_json(content, file):
 		content (dict): Content to be written to file.
 		file (string): Path to file to be written, including desired name of file (with extension).
 	"""
-	with open(file, 'wb') as json_file:
+	with open(file, 'w') as json_file:
 		json.dump(content, json_file, indent=4)
 
 if __name__ == '__main__':
 	comment_data = get_data_array(sheet)
-	comment_data = convert_countries(comment_data, 0)
+	new_comment_data = convert_countries(comment_data, 0)
 
 	download_data = get_data_array_from_csv('data/OA_Stats_Map.csv')
 	download_data = convert_countries(download_data, 0)
 	download_data = create_download_json(download_data)
 
-	json_data = add_to_json(download_data, comment_data)
+	json_data = add_to_json(download_data, new_comment_data)
 	write_to_json(json_data, 'static/comments.json')
